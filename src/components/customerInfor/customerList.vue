9df03823d833e45506964f57c9c4f5a2b5ebc24c
xyy <1762672015@qq.com>
2018-01-24 17:29:22 +0800

    客户

--- src/components/customerInfor/customerList.vue
+++ src/components/customerInfor/customerList.vue
@@ -91,11 +91,8 @@
 
             <el-col :span='19' class="border-left">
                 <el-row class="h48 pt5">
-<<<<<<< HEAD:src/components/customerInfor/customerList.vue
                     <button class="erp_bt bt_add" @click="back"><div class="btImg"><img src="../../../static/image/common/bt_add.png"></div><span class="btDetail">新增</span></button>
-=======
                     <button class="erp_bt bt_add" @click="goDetail"><div class="btImg"><img src="../../../static/image/common/bt_add.png"></div><span class="btDetail">新增</span></button>
->>>>>>> 1bfa6302172c56a869563619210a16c027b0071b:src/components/customerInfor/customerList.vue
                     <button class="erp_bt bt_excel"><div class="btImg"><img src="../../../static/image/common/bt_excel.png"></div><span class="btDetail">Excel</span></button>
                     <button class="erp_bt bt_del"><div class="btImg"><img src="../../../static/image/common/bt_del.png"></div><span class="btDetail">删除</span></button>
                     <button class="erp_bt bt_auxiliary"><div class="btImg"><img src="../../../static/image/common/bt_auxiliary.png"></div><span class="btDetail">辅助功能</span></button>
