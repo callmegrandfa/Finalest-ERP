f19bdbaae39206cba5216c43e062e3e253ec2d31
魏海东 <296656132@qq.com>
2018-04-06 17:58:53 +0800

    角色资料基本完成

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import qs from 'qs'
 
 axios.defaults.timeout = 5000;
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8;';
 axios.defaults.baseURL = 'http://192.168.100.107:8082';
 //console.log(store.state.accessToken)
 //拦截器，axios.interceptors.request.use发送请求前处理 
