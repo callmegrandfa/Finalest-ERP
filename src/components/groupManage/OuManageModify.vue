23eb404ac48a4a38d753bd9d61b87796be14ac42
unknown <1597436181@qq.com>
2018-04-20 08:47:08 +0800

    组织管理公司保存第二次无效果bug修改

--- src/components/groupManage/OuManageModify.vue
+++ src/components/groupManage/OuManageModify.vue
@@ -1523,7 +1523,7 @@ export default({
                         _this.ifModify=true
                     }
                 }else{
-                    _this.ifModify=false;
+                    _this.ifModify=true;
                 }
             },
             deep:true,
